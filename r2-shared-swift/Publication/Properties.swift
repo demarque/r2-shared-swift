e59d483f526fed3c328def4d2ede437c0d95a2fd
ilukes <lukes.lu@yahoo.com>
2020-10-20 17:07:27 +0800

    Link & Properties inherties Hashable for use in DiffableDataSource

--- r2-shared-swift/Publication/Properties.swift
+++ r2-shared-swift/Publication/Properties.swift
@@ -14,7 +14,7 @@ import Foundation
 
 /// Link Properties
 /// https://readium.org/webpub-manifest/schema/properties.schema.json
-public struct Properties: Equatable, Loggable, WarningLogger {
+public struct Properties: Equatable, Loggable, WarningLogger, Hashable {
     
     /// Additional properties for extensions.
     public var otherProperties: [String: Any] { otherPropertiesJSON.json }
